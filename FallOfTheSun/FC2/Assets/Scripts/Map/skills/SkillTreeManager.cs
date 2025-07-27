cb9d1a4eec49793c4dc6c43d73363ea6907dc106
LukaszSedkowski <147032600+LukaszSedkowski@users.noreply.github.com>
2025-07-27 19:57:05 +0200

    a lot small map changes

--- FallOfTheSun/FC2/Assets/Scripts/Map/skills/SkillTreeManager.cs
+++ FallOfTheSun/FC2/Assets/Scripts/Map/skills/SkillTreeManager.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using System.Collections;
 
 public class SkillTreeManager : MonoBehaviour
 {
     public SkillNode rootNode; // <- tu przypisz pierwszy przycisk w Inspectorze
 
-    void Start()
+    IEnumerator Start()
     {
         if (rootNode != null)
         {
             rootNode.Unlock(); // odblokuj pierwszy przycisk
+            yield return null; // poczekaj jedną klatkę, aż wszystkie linie się narysują
+            rootNode.ForceClick(); // teraz kliknij – linie będą już istnieć
         }
     }
 }
